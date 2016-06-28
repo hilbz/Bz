f67dd8df8bef9ff34b7243d37392fba5245ccd3f
Lbz <719860208@qq.com>
2016-06-28 17:53:09 +0800

    automapper

---
+++ Bz/Bz.AutoMapper/AutoMapper/Startup/BzAutoMapperModule.cs
@@ -0,0 +1,14 @@
+﻿using Bz.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bz.AutoMapper.AutoMapper.Startup
+{
+    [DependsOn(typeof(BzKernelModule))]
+    public class BzAutoMapperModule:BzModule
+    {
+    }
+}
---
+++ Bz/SimpleDemo.Web/Models/AutoMapperTest/FromEntity.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+
+namespace SimpleDemo.Web.Models
+{
+    public class SourceEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+
+    public class DestEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+
+    public static class InitailAutoMapper
+    {
+        public static void Initail()
+        {
+            Mapper.CreateMap<SourceEntity, DestEntity>();
+        }
+    }
+}
